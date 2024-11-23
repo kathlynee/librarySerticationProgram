589ac64a5fd54663b616a391597646cbd5ce3055
kathlynee <ksarahfania@student.ciputra.ac.id>
2024-11-23 17:51:48 +0700

    Initial Commit

---
+++ librarySerticationProgram/librarySerticationProgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  librarySerticationProgramApp.swift
+//  librarySerticationProgram
+//
+//  Created by Kathlyne Sarah on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct librarySerticationProgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
